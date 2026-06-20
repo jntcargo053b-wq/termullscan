184b933052e9ba875f7aae17e21633c903de0e83
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-20 15:24:54 +0700

    Create layout_metrics.dart

---
+++ lib/watermark/layouts/layout_metrics.dart
@@ -0,0 +1,25 @@
+class LayoutMetrics {
+  final double baseSize;
+  final double padding;
+  final double fontSize;
+  final double lineHeight;
+  final double stripHeight;
+  final double logoMaxSize;
+  final int textRowCount;
+  final double canvasWidth;
+  final double canvasHeight;
+  final double textAvailableWidth;
+
+  const LayoutMetrics({
+    required this.baseSize,
+    required this.padding,
+    required this.fontSize,
+    required this.lineHeight,
+    required this.stripHeight,
+    required this.logoMaxSize,
+    required this.textRowCount,
+    required this.canvasWidth,
+    required this.canvasHeight,
+    required this.textAvailableWidth,
+  });
+}
