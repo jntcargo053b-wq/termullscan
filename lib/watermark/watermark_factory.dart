03cb88339f88eddb32b7d10aec2c3dc1dc8ceeaf
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-20 16:09:10 +0700

    Update watermark_factory.dart

--- lib/watermark/watermark_factory.dart
+++ lib/watermark/watermark_factory.dart
@@ -4,26 +4,8 @@ import 'layouts/polaroid_layout.dart';
 import 'layouts/minimal_layout.dart';
 import 'layouts/professional_layout.dart';
 import 'layouts/stamp_layout.dart';
-import 'renderers/base_renderer.dart';
-import 'renderers/polaroid_renderer.dart';
-import 'renderers/minimal_renderer.dart';
-import 'renderers/professional_renderer.dart';
-import 'renderers/stamp_renderer.dart';
 
 class WatermarkFactory {
-  static WatermarkRenderer createRenderer(WatermarkStyle style) {
-    switch (style) {
-      case WatermarkStyle.polaroid:
-        return PolaroidRenderer();
-      case WatermarkStyle.minimal:
-        return MinimalRenderer();
-      case WatermarkStyle.professional:
-        return ProfessionalRenderer();
-      case WatermarkStyle.stamp:
-        return StampRenderer();
-    }
-  }
-
   static WatermarkLayout createLayout(WatermarkStyle style) {
     switch (style) {
       case WatermarkStyle.polaroid:
