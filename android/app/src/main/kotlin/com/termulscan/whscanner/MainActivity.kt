931212d9a8decdf1cf180d63bfc242ce23320ea7
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-21 20:15:53 +0700

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/termulscan/whscanner/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.termulscan.whscanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    // Semua plugin (geolocator, permission_handler, image_picker, mobile_scanner, dll.)
+    // akan didaftarkan secara otomatis oleh Flutter Engine.
+    // Tidak diperlukan kode tambahan di sini.
+}
