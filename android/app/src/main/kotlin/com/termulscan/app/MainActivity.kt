2243c7251a165dc5d0620c70f3f0fa54768e477e
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-21 20:09:09 +0700

    Delete android/app/src/main/kotlin/com/termulscan/app/MainActivity.kt

--- android/app/src/main/kotlin/com/termulscan/app/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.termulscan.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-    // Semua plugin (geolocator, permission_handler, image_picker, mobile_scanner, dll.)
-    // akan didaftarkan secara otomatis oleh Flutter Engine.
-    // Tidak diperlukan kode tambahan di sini.
-}
