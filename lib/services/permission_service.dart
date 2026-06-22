88c96760acf6f739a78a1921a592e055ef1df6ad
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-22 12:15:32 +0700

    Update permission_service.dart

--- lib/services/permission_service.dart
+++ lib/services/permission_service.dart
@@ -1,11 +1,17 @@
-import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionService {
   static Future<bool> isAndroid13OrHigher() async {
-    if (!Platform.isAndroid) return false;
     final info = await DeviceInfoPlugin().androidInfo;
     return info.version.sdkInt >= 33;
+  }
+
+  static Future<bool> requestGalleryPermission() async {
+    if (await isAndroid13OrHigher()) {
+      return (await Permission.photos.request()).isGranted;
+    }
+
+    return (await Permission.storage.request()).isGranted;
   }
 }
