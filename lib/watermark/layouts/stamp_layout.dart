8ac4b477af281b983647d9ced11d14a77db599c8
jntcargo053b-wq <jntcargo053b@gmail.com>
2026-06-23 22:42:10 +0700

    Update stamp_layout.dart

--- lib/watermark/layouts/stamp_layout.dart
+++ lib/watermark/layouts/stamp_layout.dart
@@ -7,8 +7,8 @@ import 'package:intl/intl.dart';
 import 'base_layout.dart';
 import 'layout_metrics.dart';
 import '../models/watermark_data.dart';
-import '../models/watermark_style.dart';
-import '../watermark_settings.dart'; // ✅ import
+import '../watermark_style.dart'; // ✅ import yang benar
+import '../watermark_settings.dart';
 import '../helpers/layout_helper.dart';
 import '../helpers/text_helper.dart';
 import '../widgets/logo_widget.dart';
